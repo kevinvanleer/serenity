bee747945c3093c0719f0a0d7a48cdc6a1dd857a
Kevin Van Leer <kevin.vanleer@gmail.com>
2023-09-26 07:03:55 -0500

    Renamed app to Serenity, stubbed out sleep timer

--- app/src/main/java/com/kvl/serenity/MainActivity.kt
+++ app/src/main/java/com/kvl/serenity/MainActivity.kt
@@ -7,12 +7,10 @@ import android.os.PowerManager
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -21,7 +19,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.rounded.Pause
 import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material3.Button
@@ -33,7 +30,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
@@ -76,15 +72,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         mediaPlayer = MediaPlayer.create(applicationContext, waveFile)
         mediaPlayer.setWakeMode(applicationContext, PowerManager.PARTIAL_WAKE_LOCK)
-        val shaper = mediaPlayer.createVolumeShaper(VolumeShaper.Configuration.Builder()
-            .setDuration(VOLUME_RAMP_TIME)
-            .setCurve(
-                //generateSequence(0f) { (it + 0.1f).takeUnless { new -> new > 1f } }.toMutableList().apply{add(1f)}.toFloatArray(),
-                //generateSequence(0f) { (it + 0.1f).takeUnless { new -> new > 1f } }.toMutableList().apply{add(1f)}.toFloatArray(),
-            floatArrayOf(0f,1f), floatArrayOf(0f,1f)
-            )
-            .setInterpolatorType(VolumeShaper.Configuration.INTERPOLATOR_TYPE_LINEAR)
-            .build())
+        val shaper = mediaPlayer.createVolumeShaper(
+            VolumeShaper.Configuration.Builder()
+                .setDuration(VOLUME_RAMP_TIME)
+                .setCurve(
+                    //generateSequence(0f) { (it + 0.1f).takeUnless { new -> new > 1f } }.toMutableList().apply{add(1f)}.toFloatArray(),
+                    //generateSequence(0f) { (it + 0.1f).takeUnless { new -> new > 1f } }.toMutableList().apply{add(1f)}.toFloatArray(),
+                    floatArrayOf(0f, 1f), floatArrayOf(0f, 1f)
+                )
+                .setInterpolatorType(VolumeShaper.Configuration.INTERPOLATOR_TYPE_LINEAR)
+                .build()
+        )
         val isPlaying = mutableStateOf(false)
         val buttonEnabled = mutableStateOf(true)
         /*mediaPlayer.setOnPreparedListener {
@@ -100,7 +98,10 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     App(
-                        buttonEnabled = buttonEnabled.value, isPlaying = isPlaying.value, timeRemaining = null, onClick = {
+                        buttonEnabled = buttonEnabled.value,
+                        isPlaying = isPlaying.value,
+                        timeRemaining = null,
+                        onClick = {
                             isPlaying.value = !mediaPlayer.isPlaying
                             when (mediaPlayer.isPlaying) {
                                 true -> {
@@ -112,13 +113,14 @@ class MainActivity : ComponentActivity() {
                                         buttonEnabled.value = true
                                     }
                                 }
+
                                 else -> {
                                     mediaPlayer.start()
                                     shaper.apply(VolumeShaper.Operation.PLAY)
                                 }
                             }
                         },
-                        startSleepTimer = {sleepTime: Int ->
+                        startSleepTimer = { sleepTime: Int ->
                             createSleepTimer(sleepTime)
                         }
                     )
@@ -141,26 +143,31 @@ fun Greeting() {
     Column(
         Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = "Serenity",
-            fontSize = 8.em
+            fontSize = 10.em
         )
         Spacer(modifier = Modifier.height(24.dp))
-        Text(text = "Roaring Fork",
-                fontSize = 6.em
+        Text(
+            text = "Roaring Fork",
+            fontSize = 6.em
         )
-        Text(text = "The Great Smoky Mountains",
+        Text(
+            text = "The Great Smoky Mountains",
             fontSize = 4.em
         )
     }
 }
 
 @Composable
-fun PlayPauseButton( onClick: () -> Unit = {}, isPlaying: Boolean, enabled: Boolean = true) {
-    Button(enabled = enabled, onClick = onClick, modifier = Modifier
-        .aspectRatio(1f, false)) {
-        when(isPlaying) {
+fun PlayPauseButton(onClick: () -> Unit = {}, isPlaying: Boolean, enabled: Boolean = true) {
+    Button(
+        enabled = enabled, onClick = onClick, modifier = Modifier
+            .aspectRatio(1f, false)
+    ) {
+        when (isPlaying) {
             false -> Icon(
                 androidx.compose.material.icons.Icons.Rounded.PlayArrow,
                 contentDescription = "Start playing sound",
@@ -168,6 +175,7 @@ fun PlayPauseButton( onClick: () -> Unit = {}, isPlaying: Boolean, enabled: Bool
                     .width(Dp(64f))
                     .height(Dp(64f))
             )
+
             else -> Icon(
                 androidx.compose.material.icons.Icons.Rounded.Pause,
                 contentDescription = "Pause sound",
@@ -188,7 +196,7 @@ fun GreetingPreview() {
 }
 
 class BooleanParameterProvider : PreviewParameterProvider<Boolean> {
-    override val values = sequenceOf(true,false)
+    override val values = sequenceOf(true, false)
 }
 
 @Preview(showBackground = true)
@@ -203,31 +211,71 @@ fun PlayPauseButtonPreview(
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun App(onClick: () -> Unit, startSleepTimer: (time: Int) -> Unit, timeRemaining: Int? = null, isPlaying: Boolean, buttonEnabled: Boolean) {
+fun App(
+    onClick: () -> Unit,
+    startSleepTimer: (time: Int) -> Unit,
+    timeRemaining: Int? = null,
+    isPlaying: Boolean,
+    buttonEnabled: Boolean
+) {
     Column(Modifier.fillMaxSize()) {
-        Box(contentAlignment = Alignment.Center,
-            modifier = Modifier.weight(1f).fillMaxWidth()) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        ) {
             Greeting()
         }
-        Box(contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxWidth().weight(1f)) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
             PlayPauseButton(enabled = buttonEnabled, onClick = onClick, isPlaying = isPlaying)
         }
-        Box(contentAlignment = Alignment.CenterStart,
-            modifier = Modifier.weight(1f).fillMaxWidth().padding(16.dp)) {
+        Box(
+            contentAlignment = Alignment.CenterStart,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
             Column {
                 Text("Sleep timer")
-                when(timeRemaining != null) {
+                when (timeRemaining != null) {
                     true -> Text("Show time remaining")
-                    else -> FlowRow(
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                        modifier = Modifier.padding(10.dp)
-                    ) {
-                        Button(onClick = {startSleepTimer(15)}) { Text("15 minutes") }
-                        Button(onClick = {startSleepTimer(30)}) { Text("30 minutes") }
-                        Button(onClick = {startSleepTimer(45)}) { Text("45 minutes") }
-                        Button(onClick = {startSleepTimer(60)}) { Text("1 hour") }
-                        Button(onClick = {startSleepTimer(120)}) { Text("2 hours") }
+                    else -> {
+                       Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            modifier = Modifier
+                                .padding(horizontal = 10.dp)
+                                .padding(top = 10.dp)
+                        ) {
+                            Button(
+                                modifier = Modifier.weight(1f),
+                                onClick = { startSleepTimer(15) }) { Text("15 min") }
+                            Button(
+                                modifier = Modifier.weight(1f),
+                                onClick = { startSleepTimer(30) }) { Text("30 min") }
+                            Button(
+                                modifier = Modifier.weight(1f),
+                                onClick = { startSleepTimer(45) }) { Text("45 min") }
+                        }
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            modifier = Modifier
+                                .padding(horizontal = 10.dp)
+                                .padding(top = 10.dp)
+                        ) {
+                            Button(
+                                modifier = Modifier.weight(1f),
+                                onClick = { startSleepTimer(60) }) { Text("1 hour") }
+                            Button(
+                                modifier = Modifier.weight(1f),
+                                onClick = { startSleepTimer(120) }) { Text("2 hours") }
+                        }
                     }
                 }
             }
