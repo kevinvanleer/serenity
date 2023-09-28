1e893fb50087cca9944c1be15f8eb2b62ead3592
Kevin Van Leer <kevin.vanleer@gmail.com>
2023-09-28 10:55:22 -0500

    Disabled dynamic color

--- app/src/main/java/com/kvl/serenity/ui/theme/Theme.kt
+++ app/src/main/java/com/kvl/serenity/ui/theme/Theme.kt
@@ -57,7 +57,7 @@ val mooli = FontFamily(
 fun SerenityTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
